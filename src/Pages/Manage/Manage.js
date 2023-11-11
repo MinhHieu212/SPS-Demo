00198734357b8d3ba066430f2dc658062e030ee8
truongthuanhung <hung.truongthuan@hcmut.edu.vn>
2023-11-11 15:27:56 +0700

    fix bugs

--- src/Pages/Manage/Manage.js
+++ src/Pages/Manage/Manage.js
@@ -47,17 +47,7 @@ const ManageStaff = () => {
       <h2 className="text-3xl lg:text-4xl font-semibold mt-4 printing-title border-b-4 border-black pb-2 md:pb-3  text-[#066DCC] ">
         QUẢN LÝ MÁY IN
       </h2>
-      <div className="flex flex-col md:flex-row mt-4 items-center gap-4">
-        <div className="w-full md:w-1/2 relative">
-          <input
-            type="text"
-            placeholder="Tìm theo ID máy in"
-            className="w-full lg:w-[90%] border border-black"
-          />
-          <div className="absolute right-[3%] md:right-[6%] lg:right-[12%] bottom-1/2 translate-y-1/2">
-            <SearchIcon></SearchIcon>
-          </div>
-        </div>
+      <div className="flex flex-col-reverse md:flex-row mt-4 items-center gap-4">
         <div className="w-full md:w-1/2 relative shadow-md">
           <div className="bg-[#3C8DBC] text-white text-xl font-bold flex flex-row justify-center items-center text-center py-[14px] px-[8px]">
             <p className="text-base lg:text-xl w-1/2">TỔNG SỐ</p>
@@ -66,6 +56,16 @@ const ManageStaff = () => {
           <div className="bg-white flex flex-row text-base font-bold justify-center items-center text-center py-[14px]">
             <p className="text-base lg:text-xl w-1/2">{totalPrinters}</p>
             <p className="text-base lg:text-xl w-1/2">{onlinePrinters}</p>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 relative">
+          <input
+            type="text"
+            placeholder="Tìm theo ID máy in"
+            className="w-full lg:w-[90%] border border-black"
+          />
+          <div className="absolute right-[3%] md:right-[6%] lg:right-[12%] bottom-1/2 translate-y-1/2">
+            <SearchIcon></SearchIcon>
           </div>
         </div>
       </div>
