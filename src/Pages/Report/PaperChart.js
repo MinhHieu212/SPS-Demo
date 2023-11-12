a553fb4cf9626851bc3030a4c14ab97ad87a831c
MinhHieu212 <hieu.tranminhk21@hcmut.edu.vn>
2023-11-12 12:03:30 +0700

    Review and asyn Spso UI

--- src/Pages/Report/PaperChart.js
+++ src/Pages/Report/PaperChart.js
@@ -1,72 +1,74 @@
-import React from "react";
-import { Bar } from 'react-chartjs-2';
+import React, { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 function PaperChart({ data }) {
-    const chartData = {
-        labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ],
-        datasets: [
-            {
-                label: "TỜ A4",
-                backgroundColor: [
-                    "#3e95cd"
-                ],
-                data: data,
-                borderWidth: 1,
-            },
+  const chartData = {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+    datasets: [
+      {
+        label: "TỜ A4",
+        backgroundColor: ["#3e95cd"],
+        data: data,
+        borderWidth: 1,
+      },
+    ],
+  };
+  const chartOptions = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          fontSize: 14, // Adjust the font size here
+        },
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "LƯỢNG GIẤY ĐÃ IN",
+        color: "#1488db",
+        font: {
+          size: 18,
+        },
+      },
+    },
+  };
 
-        ]
-    };
-    const chartOptions = {
-        scales: {
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    fontSize: 14, // Adjust the font size here
-                },
-            },
-        },
-        plugins: {
-            title: {
-                display: true,
-                text: 'LƯỢNG GIẤY ĐÃ IN',
-                color: '#1488db',
-                font: {
-                    size: 18,
-                },
-            },
-        },
-    };
-    return <Bar data={chartData} options={chartOptions} />;
+  const chartMinHeight = window.innerWidth <= 600 ? 300 : undefined;
+
+  return (
+    <Bar data={chartData} options={chartOptions} height={chartMinHeight} />
+  );
 }
 
 export default PaperChart;
--- src/Pages/Report/Report.js
+++ src/Pages/Report/Report.js
@@ -10,7 +10,7 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
+    maintenance: 10,
   },
   {
     time: "Tháng 1/2021",
@@ -19,7 +19,7 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
+    maintenance: 10,
   },
   {
     time: "Tháng 1/2021",
@@ -28,7 +28,7 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
+    maintenance: 10,
   },
   {
     time: "Tháng 1/2021",
@@ -37,7 +37,7 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
+    maintenance: 10,
   },
   {
     time: "Tháng 1/2021",
@@ -46,7 +46,7 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
+    maintenance: 10,
   },
   {
     time: "Tháng 1/2021",
@@ -55,7 +55,7 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
+    maintenance: 10,
   },
   {
     time: "Tháng 1/2021",
@@ -64,7 +64,7 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
+    maintenance: 10,
   },
   {
     time: "Tháng 1/2021",
@@ -73,44 +73,80 @@ const items = [
     frequency: 80,
     a3: 50,
     a4: 100,
-    maintenance: 10
-  }
-]
+    maintenance: 10,
+  },
+];
 const Report = () => {
   return (
-    <div className="Activities max-w-[1280px] px-[10px] md:px-[32px] lg:px-[70px] bg-[white] shadow-sm mb-5 min-h-[93vh]">
+    <div className="Activities mx-auto max-w-[1280px] px-[10px] md:px-[32px] lg:px-[70px] bg-[white] shadow-sm mb-5 min-h-[93vh]">
       <h2 className="text-3xl lg:text-4xl font-semibold mt-8 printing-title border-b-4 border-black pb-2 md:pb-3  text-[#066DCC] mb-4">
         BÁO CÁO HỆ THỐNG
       </h2>
-      <PaperChart
-        data = {data}
-      />
-      <h2 className="text-base lg:text-xl font-bold underline px-8 mt-[93px] mb-4">TẤT CẢ MÁY IN</h2>
-      <div className="flex flex-col lg:flex-row px-8 gap-4">
-        <div className="w-full lg:w-1/2 flex flex-col gap-4">
-          <p className="text-base lg:text-xl text-black font-semibold"><span className="text-base lg:text-xl text-[#1488db] font-semibold">Tổng lượng giấy đã in: </span>2000</p>
-          <p className="text-base lg:text-xl text-black font-semibold"><span className="text-base lg:text-xl text-[#1488db] font-semibold">Tổng lượng giấy A3 đã in: </span>120</p>
-          <p className="text-base lg:text-xl text-black font-semibold"><span className="text-base lg:text-xl text-[#1488db] font-semibold">Tổng lượng giấy A4 đã in: </span>1880</p>
-          <p className="text-base lg:text-xl text-black font-semibold"><span className="text-base lg:text-xl text-[#1488db] font-semibold">Tổng số lần bảo trì: </span>13</p>
+      <PaperChart data={data} />
+      <h2 className="text-[16px] md:text-[20px]  font-bold underline px-8 mt-[50px] mb-4">
+        TẤT CẢ MÁY IN
+      </h2>
+      <div className="flex flex-col md:flex-row px-8 gap-3  ">
+        <div className="w-full lg:w-1/2 flex flex-col gap-3 ">
+          <p className="text-[16px] md:text-[20px]  text-black font-semibold">
+            <span className="   text-[#1488db] font-semibold">
+              Tổng lượng giấy đã in:{" "}
+            </span>
+            2000
+          </p>
+          <p className="text-[16px] md:text-[20px]  text-black font-semibold">
+            <span className="  text-[#1488db] font-semibold">
+              Tổng lượng giấy A3 đã in:{" "}
+            </span>
+            120
+          </p>
+          <p className="text-[16px] md:text-[20px]  text-black font-semibold">
+            <span className="   text-[#1488db] font-semibold">
+              Tổng lượng giấy A4 đã in:{" "}
+            </span>
+            1880
+          </p>
+          <p className="text-[16px] md:text-[20px]  text-black font-semibold">
+            <span className="  text-[#1488db] font-semibold">
+              Tổng số lần bảo trì:{" "}
+            </span>
+            13
+          </p>
         </div>
-        <div className="w-full lg:w-1/2 flex flex-col gap-4">
-        <p className="text-sm lg:text-xl text-black font-semibold"><span className="text-base lg:text-xl text-[#1488db] font-semibold">Trung bình lượng giấy A3 trên mỗi máy: </span>40</p>
-          <p className="text-sm lg:text-xl text-black font-semibold"><span className="text-base lg:text-xl text-[#1488db] font-semibold">Trung bình lượng giấy A4 trên mỗi máy: </span>120</p>
-          <p className="text-sm lg:text-xl text-black font-semibold"><span className="text-base lg:text-xl text-[#1488db] font-semibold">Trung bình số lần in trên mỗi máy: </span>20</p>
+        <div className="w-full lg:w-1/2 flex flex-col gap-3 ">
+          <p className="text-[16px] md:text-[20px]  text-black font-semibold">
+            <span className="   text-[#1488db] font-semibold">
+              Trung bình lượng giấy A3 trên mỗi máy:{" "}
+            </span>
+            40
+          </p>
+          <p className="text-[16px] md:text-[20px]  text-black font-semibold">
+            <span className="   text-[#1488db] font-semibold">
+              Trung bình lượng giấy A4 trên mỗi máy:{" "}
+            </span>
+            120
+          </p>
+          <p className="text-[16px] md:text-[20px]  text-black font-semibold">
+            <span className="   text-[#1488db] font-semibold">
+              Trung bình số lần in trên mỗi máy:{" "}
+            </span>
+            20
+          </p>
         </div>
       </div>
       <div className="w-full overflow-x-auto">
         <div className="flex flex-row justify-between items-center bg-[#3C8DBC] text-base lg:text-lg font-bold py-3 px-4 mt-8 rounded-sm w-[714px] md:w-full text-white">
-          <div className="min-w-[10%] text-center">THỜI GIAN</div>
+          <div className="min-w-[15%] text-center">THỜI GIAN</div>
           <div className="min-w-[15%] text-center">ID MÁY IN</div>
           <div className="min-w-[15%] text-center">VỊ TRÍ</div>
           <div className="min-w-[15%] text-center">SỐ LẦN IN</div>
           <div className="min-w-[15%] text-center">GIẤY A3 ĐÃ IN</div>
           <div className="min-w-[15%] text-center">GIẤY A4 ĐÃ IN</div>
-          <div className="min-w-[15%] text-center">BẢO TRÌ</div>
+          <div className="min-w-[10%] text-center">BẢO TRÌ</div>
         </div>
-        {items.map(item => (
+        {items.map((item, index) => (
           <ReportItem
+            key={index}
             time={item.time}
             id={item.id}
             location={item.location}
