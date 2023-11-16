93118efdcb55d8d6eb975501803d57008dfd8c6e
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2023-11-16 23:10:06 +0700

    config

--- src/Pages/Config/Config.js
+++ src/Pages/Config/Config.js
@@ -1,14 +1,13 @@
-import React, { useRef, useState } from "react";
-import {FormInput} from "./FormInput.js"
-import { number } from "yup";
+import React, { useState } from "react";
 import "./Config.scss"
+import {AiOutlineDelete} from 'react-icons/ai'
 
 const Config = () => {
-
+  // Attributes for the first form
   const [values, setValues] = useState({
     default_paper: 100,
-    time_Sem1: 12,
-    time_Sem2: 14,
+    time_Sem1: "22-12-2021",
+    time_Sem2: "22-12-2022",
     a4_price: 300
   });
   const inputs = [
@@ -16,10 +15,9 @@ const Config = () => {
       id: 1,
       name: "default_paper",
       type: "number",
-      placeholder: "defaultPaper",
+      placeholder: "Nhập số...",
       errorMessage: "Lượng giấy mặc định là số tự nhiên từ 1-1000!",
       label: "Số lượng giấy mặc định cho từng sinh viên / Học kì",
-      require: true,
     },
     {
       id: 2,
@@ -28,7 +26,6 @@ const Config = () => {
       placeholder: "timeSem1",
       errorMessage: "",
       label: "Thời gian cấp giấy Học kì 1",
-      require: true,
     },
     {
       id: 3,
@@ -37,42 +34,150 @@ const Config = () => {
       placeholder: "timeSem2",
       errorMessage: "",
       label: "Thời gian cấp giấy Học kì 2",
-      require: true,
     },
     {
       id: 4,
       name: "a4_price",
       type: "number",
-      placeholder: "a4Price",
+      placeholder: "Nhập giá...",
       errorMessage: "Giá một tờ A4 không nên vượt quá 2000đ",
       label: "Giá của một tờ giấy A4 khi mua thêm",
-      require: true,
     },
   ];
   const handleSubmit = (e) => {
+    setValues({...values, [e.target.name]: e.target.value});
     e.preventDefault();
-    // const data = new FormData(e.target);
-    // console.log(Object.fromEntries(data.entries()));
   };
   const onChange = (e) => {
     setValues({...values, [e.target.name]: e.target.value});
   }
+  const setDefault = () => {
+    setValues({
+      default_paper: 100,
+      time_Sem1: new Date('09-22-2021'),
+      time_Sem2: new Date('09-22-2021'),
+      a4_price: 300
+    });
+  };
+
+  // Attributes for the second form
+  const [allItem,setAllItem] = useState([]);
+  const [selectedValue, setSelectedValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const updateInputType = (e) => {
+      setSelectedValue(e.target.value);
+  }
+  const handleAdd = (val) => {
+    if (val !== "") {
+      if (!allItem.some(element => element.fileType === val)) 
+      {
+        let newitem = {
+          id: allItem.length === 0? 1 : allItem[allItem.length - 1].id + 1,
+          fileType: selectedValue,
+        } 
+        setAllItem([...allItem, newitem]);
+      }
+      else {
+        setErrorMessage("Loại file đã tồn tại.");
+      }
+    }
+  }
+
+  const handleDelete = (id) => {
+    const newAllItem = allItem.filter((item) => item.id !== id);
+    setAllItem(newAllItem);
+  }
   return (
     <div className="Config max-w-[1280px] px-[15px] md:px-[32px] lg:px-[70px] mx-auto mb-5 bg-[#E5E5E5] shadow-sm min-h-[93vh]">
-      <h1 className="text-3xl lg:text-4xl font-semibold mt-4 border-b-4 border-black pb-2 md:pb-3">
+      <h1 className="text-3xl lg:text-4xl text-[#066dcc] font-semibold mt-4 border-b-4 border-black pb-2 md:pb-3">
         CẤU HÌNH HỆ THỐNG
       </h1>
-      <div className="Form_quanlyvatlieu flex item-center justify-center bg-red justify-between">
-        <form onSubmit={handleSubmit}>
-        <h2 className="text-xl lg:text-2xl font-semibold mt-4 border-b-4 pb-2 md:pb-3">Quản lý Vật Liệu Học Tập và Cấp Phát Tài Liệu</h2>
-          {inputs.map((input) => (
-              <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
-          ))}
-          <button className="Submit">Áp dụng</button>
-          <button className="Todefault">Mặc định</button>
+      <div className="Form_quanlyvatlieu flex item-center justify-center bg-cover bg-center mx-auto my-10 ">
+        <form onSubmit={handleSubmit} className="bg-white py-0 px-[3.75rem] w-[35.625rem]">
+          <h2 className="text-xl lg:text-2xl text-[#1488DB] text-center font-semibold mt-4 border-b-4 border-[#1488DB] pb-2 md:pb-3 mb-2 md:mb-6">Quản lý Vật Liệu Học Tập và Cấp Phát Tài Liệu</h2>
+            <div className="formInput flex flex-col">
+              <label>{inputs[0].label}</label>
+              <div>
+                  <input placeholder={inputs[0].placeholder} onChange={onChange}/> 
+              </div>
+              <span className="Err">{errorMessage}</span>
+            </div>
+
+            <div className="formInput flex flex-col">
+              <label>{inputs[1].label}</label>
+              <div>
+                  <input type="date" name="date" id="datepicker" required onChange={onChange}/> 
+              </div>
+              <span className="Err">{errorMessage}</span>
+            </div>
+
+            <div className="formInput flex flex-col">
+              <label>{inputs[2].label}</label>
+              <div>
+                  <input type="date" name="date" id="datepicker" required onChange={onChange}/> 
+              </div>
+              <span className="Err">{errorMessage}</span>
+            </div>
+            
+            <div className="formInput flex flex-col">
+              <label>{inputs[3].label}</label>
+              <div>
+                  <input placeholder={inputs[3].placeholder} onChange={onChange}/> 
+                  <span className="font-bold text-xl w-1/3 "> (vnđ)</span>
+              </div>
+              <span className="Err">{errorMessage}</span>
+            </div>
+          <button className="Submit flex float-right items-center justify-center my-4 mx-[0.5rem]" type="submit">Áp dụng</button>
+          <button className="Todefault flex float-right items-center justify-center my-4 mx-[0.5rem]" type="button" onClick={setDefault}>Mặc định</button>
         </form>
       </div>
       
+
+      <div className="Form_fileduocphep flex item-center justify-center bg-cover bg-center mx-auto my-10">
+          <form onSubmit={handleSubmit} className="bg-white py-0 px-[3.75rem] w-[35.625rem]">
+            <h2 className="text-xl lg:text-2xl text-[#1488DB] font-semibold mt-4 border-b-4 border-[#1488DB] pb-2 md:pb-3">Cấu hình các loại file được in</h2>
+            <div className="selectlist my-[1.5rem] mx-0" > 
+              <select className="w-[60%] h-[2.5rem] py-0 px-2 rounded-[0.1875rem]" onChange={updateInputType}>
+                <option selected = "true" disabled="disable" value="">Chọn loại tệp để thêm...</option>
+                <option value=".pptx">.pptx</option>
+                <option value=".pdf">.pdf</option>
+                <option value=".png">.png</option>
+                <option value=".doc">.doc</option>
+                <option value=".docx">.docx</option>
+                <option value=".jpg">.jpg</option>
+                <option value=".xlsx">.xlsx</option>
+              </select>
+
+              <button className="float-right" type="submit" onClick={() => handleAdd(selectedValue)}>Thêm</button>
+              {errorMessage && <div>
+                {errorMessage}
+                {setTimeout(() => {setErrorMessage(null)},3000)}
+                </div>}
+            </div>
+
+            <h3 className="text-xs lg:text-xl font-semibold mt-4 pb-2 md:pb-3 relative"><span className="border-b-2 border-black">Loại file được phép in trong hệ thống</span></h3>
+            <div className="currentlist_wrapper scroll shadow-md shadow-slate-600 mb-[1.3125rem]" style={{ overflowY: 'auto', maxHeight: '12rem' }}>
+              <div className="current_list flex flex-col">
+                {(() => {
+                    if (allItem.length === 0) {
+                      return <div className="italic text-gray-400">Bạn hiện chưa thêm loại file nào.</div>;
+                    }
+                })()}
+                {allItem.map((item, key) => {
+                  return (
+                    <div className="current_list_item flex flex-row my-[0.5rem] py-[0.5rem] px-[1rem]" key={key}>
+                      <p>{item.fileType}</p>
+                      <div>
+                        <AiOutlineDelete className="icon text-2xl cursor-pointer" onClick={()=>handleDelete(item.id)} title="Delete?"/>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+           </div>
+
+          </form>
+      </div>
     </div>
   );
 };
