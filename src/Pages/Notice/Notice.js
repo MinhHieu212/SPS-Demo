74c43cb1eed3ebd150e9501ca74a7ca229a66a66
truongthuanhung <hung.truongthuan@hcmut.edu.vn>
2023-12-05 22:36:57 +0700

    save code

--- src/Pages/Notice/Notice.js
+++ src/Pages/Notice/Notice.js
@@ -3,23 +3,37 @@ import NoticeItem from "./NoticeItem";
 import { getNotice } from "../../APIs/SpsoAPI/SpsoAPI";
 import { useNavigate } from "react-router";
 import { convertTime } from "../../Utils/Time";
-export var newNotifies = 6;
+import { useSocket } from "../../Contexts/SocketIOContenxt";
+
+export var newNotifies = 0;
 const Notice = () => {
   const navigate = useNavigate();
+  const socket = useSocket();
   const [renderList, setRenderList] = useState(true);
   const [data, setData] = useState([]);
-
+  const [newNoti, setNewNoti] = useState(0);
+  const handleGetNotice = async(params) => {
+    const response = await getNotice(params);
+    console.log(response?.data?.data.allNotices);
+    setData(response?.data?.data.allNotices);
+    setNewNoti(response?.data?.data.news);
+  }
   useEffect(() => {
-    const handleGetNotice = async(params) => {
-      const response = await getNotice(params);
-      console.log(response?.data?.data.allNotices);
-      setData(response?.data?.data.allNotices);
-    }
     handleGetNotice({});
+
+    
+
     if (localStorage.getItem("accessToken") === null) {
       navigate("/Login");
     }
-  }, [renderList])
+  }, [renderList]);
+  const fetchNotify = async (params) => {
+    await handleGetNotice(params);
+  };
+  socket.on("update-notification-list", () => {
+    console.log("Receive update-notification-list...");
+    fetchNotify({});
+  });
   return <div className="Notice mx-auto max-w-[1280px] w-full px-[10px] lg:px-[20px] bg-[white] shadow-sm pb-5 min-h-[93vh]">
     <div className="flex flex-row mt-3 border-b-4 border-[#066DCC] pb-2 md:pb-3 mb-4 items-center justify-between">
       <h2 className="text-2xl lg:text-3xl font-semibold printing-title pb-2 md:pb-3 pt-2 text-[#066DCC] ">
@@ -28,20 +42,27 @@ const Notice = () => {
     </div>
     <div className=" w-full overflow-x-auto">
       <div className=" min-w-[800px] md:w-full max-h-[60px]  flex flex-row mx-auto justify-between shadow-lg items-center bg-[#3C8DBC] text-[16px]  lg:text-[18px] font-bold py-3 px-4 mt-8 rounded-sm  text-white">
-        <div className="text-center w-[10%]">VAI TRÒ</div>
-        <div className="text-center w-[50%]">NỘI DUNG</div>
-        <div className="text-center w-[20%]">VỊ TRÍ</div>
-        <div className="text-center w-[20%]">THỜI GIAN</div>
+        <div className="text-center w-[15%]">HỌ TÊN</div>
+        <div className="text-center w-[15%]">VAI TRÒ</div>
+        <div className="text-center w-[40%]">NỘI DUNG</div>
+        <div className="text-center w-[15%]">VỊ TRÍ</div>
+        <div className="text-center w-[15%]">THỜI GIAN</div>
       </div>
         {data?.map((item, index) => (
           <NoticeItem
-            new={0}
+            seen={item.seen}
+            new={item.news}
             key={index}
             index={index}
+            name={item.sender.lastName + " " + item.sender.firstName}
             role={item.sender.role}
-            content={""}
+            content={item.description}
             location={item.location.facility + ", " + item.location.department + ", " + item.location.room}
             time={convertTime(item.createdAt).slice(0,10) + " " + convertTime(item.createdAt).slice(11,19)}
+            functionNewNotifies = {() => {
+              newNotifies = newNoti;
+              console.log(newNotifies);
+            }}
           />
         ))}
     </div>
--- src/Pages/Notice/NoticeItem.js
+++ src/Pages/Notice/NoticeItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const NoticeItem = (props) => {
+    props.functionNewNotifies();
     return (
         <div className={
-            props.index < props.new ? 'bg-[#DAD8D8] hover:bg-white cursor-pointer NoticeItem flex flex-row justify-between items-center shadow-md text-[16px] lg:text-[18px] font-semibold  py-[12px] px-4 mt-2 rounded-sm border-b-[1px] border-black min-w-[800px] md:w-full max-h-[60px]' : 'bg-white cursor-pointer NoticeItem flex flex-row justify-between items-center shadow-md text-[16px] lg:text-[18px] font-semibold  py-[12px] px-4 mt-2 rounded-sm border-b-[1px] border-black min-w-[800px] md:w-full max-h-[60px]'
+            !props.seen  ? 'bg-[#DAD8D8] hover:bg-white cursor-pointer NoticeItem flex flex-row justify-between items-center shadow-md text-[16px] lg:text-[18px] font-semibold  py-[12px] px-4 mt-2 rounded-sm border-b-[1px] border-black min-w-[800px] md:w-full max-h-[60px]' : 'bg-white cursor-pointer NoticeItem flex flex-row justify-between items-center shadow-md text-[16px] lg:text-[18px] font-semibold  py-[12px] px-4 mt-2 rounded-sm border-b-[1px] border-black min-w-[800px] md:w-full max-h-[60px]'
         }
         >
-            <div className="text-center w-[10%]">{props.role}</div>
-            <div className="text-center w-[50%]">{props.content}</div>
-            <div className="text-center w-[20%]">{props.location}</div>
-            <div className="text-center w-[20%]">{props.time}</div>
+            <div className="text-center w-[15%]">{props.name}</div>
+            <div className="text-center w-[15%]">{props.role}</div>
+            <div dangerouslySetInnerHTML={{ __html: props.content }} className="text-center w-[40%]"></div>
+            <div className="text-center w-[15%]">{props.location}</div>
+            <div className="text-center w-[15%]">{props.time}</div>
         </div>
     )
 }
